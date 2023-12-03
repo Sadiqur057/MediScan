32cbdb2fdca87f0445fd8e97f8366ffbf7cda471
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-12-03 13:52:57 +0600

    delete method updated

--- script.js
+++ script.js
@@ -44,11 +44,7 @@ function showTable(option) {
             <td>
             <ul class="list-inline mb-0">
               <li class="list-inline-item">
-                <a href="javascript:void(0);" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit"
-                  class="px-2 text-primary"><i class="bx bx-pencil font-size-18"></i></a>
-              </li>
-              <li class="list-inline-item">
-                <a href="javascript:void(0);" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"
+                <a onclick="deleteData('${medicineKey}')" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"
                   class="px-2 text-danger"><i class="bx bx-trash-alt font-size-18"></i></a>
               </li>
             </ul>
@@ -91,12 +87,8 @@ function showTable(option) {
             <th scope="col">Action</th>
             <td>
               <ul class="list-inline mb-0">
-                <li class="list-inline-item">
-                  <a href="javascript:void(0);" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit"
-                    class="px-2 text-primary"><i class="bx bx-pencil font-size-18"></i></a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="javascript:void(0);" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"
+                <li>
+                  <a onclick="deleteData('${medicineKey}')" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"
                     class="px-2 text-danger"><i class="bx bx-trash-alt font-size-18"></i></a>
                 </li>
               </ul>
@@ -152,4 +144,9 @@ function saveData() {
     };
     localStorage.setItem(medicineKey, JSON.stringify(medicineData));
     onLoad();
+}
+
+function deleteData(key){
+  localStorage.removeItem(key);
+  onLoad();
 }
