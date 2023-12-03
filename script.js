b1f01d02ad5708f3b1e7dbcaef3c249ee41d000e
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-12-03 15:04:46 +0600

    added uploading and cropping

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 onLoad();
 function onLoad() {
-  showTable('col');
+  showTable("col");
 }
 
 function showTable(option) {
-    const tableRow = document.getElementById('table-row');
-    const tableCol = document.getElementById('table-col');
-    const activeRow = document.getElementById("nav-active-row")
-    const activeCol = document.getElementById("nav-active-col")
+  const tableRow = document.getElementById("table-row");
+  const tableCol = document.getElementById("table-col");
+  const activeRow = document.getElementById("nav-active-row");
+  const activeCol = document.getElementById("nav-active-col");
 
-    let tableBody = document.getElementById('medicineTableBody');
-    tableBody.innerHTML = '';
-    
-    rowTableContent = document.getElementById('table-col');
-    rowTableContent.innerHTML = '';
-    
-    for (let i = 0; i < localStorage.length; i++) {
-        let medicineKey = localStorage.key(i);
-        let medicineDataJSON = localStorage.getItem(medicineKey);
-        let medicineData = JSON.parse(medicineDataJSON);
-        let description = medicineData.description || '';
-        let remarks = medicineData.remarks || '';
-        let duration = medicineData.duration || '';
-        let schedule = medicineData.schedule ? medicineData.schedule.join(', ') : '';
+  let tableBody = document.getElementById("medicineTableBody");
+  tableBody.innerHTML = "";
 
-        if (option === 'col') {
-          tableRow.classList.remove('hidden');
-          tableCol.classList.add('hidden');
-          activeRow.classList.add('active')
-          activeCol.classList.remove('active');
-                  
-        let innerHTML = `
+  rowTableContent = document.getElementById("table-col");
+  rowTableContent.innerHTML = "";
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let medicineKey = localStorage.key(i);
+    let medicineDataJSON = localStorage.getItem(medicineKey);
+    let medicineData = JSON.parse(medicineDataJSON);
+    let description = medicineData.description || "";
+    let remarks = medicineData.remarks || "";
+    let duration = medicineData.duration || "";
+    let schedule = medicineData.schedule
+      ? medicineData.schedule.join(", ")
+      : "";
+
+    if (option === "col") {
+      tableRow.classList.remove("hidden");
+      tableCol.classList.add("hidden");
+      activeRow.classList.add("active");
+      activeCol.classList.remove("active");
+
+      let innerHTML = `
         <tr>
             <td>
                 <a href="#" class="text-body">${medicineKey}</a>
@@ -50,14 +52,13 @@ function showTable(option) {
             </ul>
           </td>
         </tr>`;
-    tableBody.innerHTML += innerHTML;
-          
-        } else if (option === 'row') {
-          tableRow.classList.add('hidden');
-          tableCol.classList.remove('hidden');
-          activeRow.classList.remove('active')
-          activeCol.classList.add('active')
-          let innerHTML = `
+      tableBody.innerHTML += innerHTML;
+    } else if (option === "row") {
+      tableRow.classList.add("hidden");
+      tableCol.classList.remove("hidden");
+      activeRow.classList.remove("active");
+      activeCol.classList.add("active");
+      let innerHTML = `
           <tr>
           <tr>
             <th scope="col">Name</th>
@@ -98,81 +99,103 @@ function showTable(option) {
           <tr>
             <th colspan="2" style="border: none;"></th>
           </tr>
-          `
-          rowTableContent.innerHTML += innerHTML;
-        }
+          `;
+      rowTableContent.innerHTML += innerHTML;
     }
-
   }
-  const fileInput = document.getElementById("fileInput");
-  const openCameraBtn = document.getElementById("openCameraBtn");
+}
+const fileInput = document.getElementById("fileInput");
+const openCameraBtn = document.getElementById("openCameraBtn");
 
-  openCameraBtn.addEventListener("click", function () {
-    fileInput.capture = "user";
-    fileInput.accept = "image/*";
-    fileInput.click();
-  });
+openCameraBtn.addEventListener("click", function () {
+  fileInput.capture = "user";
+  fileInput.accept = "image/*";
+  fileInput.click();
+});
 
 function showForm() {
-    const form = document.getElementById('addForm');
-      form.classList.remove('hidden');
-  }
+  const form = document.getElementById("addForm");
+  form.classList.remove("hidden");
+}
 function closeForm() {
-    const form = document.getElementById('addForm');
-      form.classList.add('hidden');
-  }
+  const form = document.getElementById("addForm");
+  form.classList.add("hidden");
+}
 function saveData() {
-    // Retrieve input values
-    let medicineKey = document.getElementById('medicine').value;
-    let description = document.getElementById('description').value;
-    let remarks = document.getElementById('remarks').value;
-    let duration = document.getElementById('duration').value;
+  // Retrieve input values
+  let medicineKey = document.getElementById("medicine").value;
+  let description = document.getElementById("description").value;
+  let remarks = document.getElementById("remarks").value;
+  let duration = document.getElementById("duration").value;
 
-    // Retrieve selected schedule checkboxes
-    let scheduleCheckboxes = document.getElementsByName('schedule[]');
-    let selectedSchedule = [];
-    scheduleCheckboxes.forEach(function (checkbox) {
-        if (checkbox.checked) {
-            selectedSchedule.push(checkbox.value);
-        }
-    });
-    let medicineData = {
-        'description': description,
-        'remarks': remarks,
-        'duration': duration,
-        'schedule': selectedSchedule,
-    };
-    localStorage.setItem(medicineKey, JSON.stringify(medicineData));
-    onLoad();
+  // Retrieve selected schedule checkboxes
+  let scheduleCheckboxes = document.getElementsByName("schedule[]");
+  let selectedSchedule = [];
+  scheduleCheckboxes.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      selectedSchedule.push(checkbox.value);
+    }
+  });
+  let medicineData = {
+    description: description,
+    remarks: remarks,
+    duration: duration,
+    schedule: selectedSchedule,
+  };
+  localStorage.setItem(medicineKey, JSON.stringify(medicineData));
+  onLoad();
 }
 
-function deleteData(key){
+function deleteData(key) {
   localStorage.removeItem(key);
   onLoad();
 }
 
-
-
-
+const imageContainer = document.querySelector("main-container");
 const image = document.getElementById('image');
-const imageContainer = document.querySelector('main-container');
 
-image.addEventListener('load', function() {
-  console.log('Image loaded');
- 
-  const cropper = new Cropper(image, {
-    aspectRatio: 0,
+
+document.getElementById('fileInput').addEventListener('change', function (event) {
+  var fileInput = event.target;
+  var file = fileInput.files[0];
+
+  if (file) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+          var imageElement = document.getElementById('image');
+          imageElement.classList.remove('hidden')
+          imageElement.src = e.target.result;
+
+          // Initialize Cropper.js if needed
+          const cropper = new Cropper(image, {
+            aspectRatio: 0,
+        });
+        document.querySelector('#btn-crop').addEventListener('click', function() {
+          var croppedImage = cropper.getCroppedCanvas().toDataURL("image/png");
+          document.getElementById('output').src = croppedImage;
+          document.querySelector(".cropped-container").style.display = 'flex';
+      });
+        
+      };
+      reader.readAsDataURL(file);
+  }
 });
 
-document.querySelector('#btn-crop').addEventListener('click', function() {
-    var croppedImage = cropper.getCroppedCanvas().toDataURL("image/png");
-    document.getElementById('output').src = croppedImage;
-    document.querySelector(".cropped-container").style.display = 'flex';
-});
-});
-image.addEventListener('error', function() {
-  image.addEventListener('error', function() {
-    console.log('Image failed to load');
-});
-});
 
+
+
+
+// image.addEventListener("load", function () {
+//   console.log("Image loaded");
+
+  // const cropper = new Cropper(image, {
+  //   aspectRatio: 0,
+  // });
+
+
+// });
+// image.addEventListener("error", function () {
+//   image.addEventListener("error", function () {
+//     console.log("Image failed to load");
+//   });
+// });
