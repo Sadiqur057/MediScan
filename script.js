a31053e2fddcd4ed9a24e9cd927ad6497f16185b
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-12-03 13:12:55 +0600

    showing data from localstorage

--- script.js
+++ script.js
@@ -3,7 +3,115 @@ function onLoad() {
   showTable('col');
 }
 
+function showTable(option) {
+    const tableRow = document.getElementById('table-row');
+    const tableCol = document.getElementById('table-col');
+    const activeRow = document.getElementById("nav-active-row")
+    const activeCol = document.getElementById("nav-active-col")
 
+    let tableBody = document.getElementById('medicineTableBody');
+    tableBody.innerHTML = '';
+    
+    rowTableContent = document.getElementById('table-col');
+    rowTableContent.innerHTML = '';
+    
+    for (let i = 0; i < localStorage.length; i++) {
+        let medicineKey = localStorage.key(i);
+        let medicineDataJSON = localStorage.getItem(medicineKey);
+        let medicineData = JSON.parse(medicineDataJSON);
+        let description = medicineData.description || '';
+        let remarks = medicineData.remarks || '';
+        let duration = medicineData.duration || '';
+        let schedule = medicineData.schedule ? medicineData.schedule.join(', ') : '';
+
+        if (option === 'col') {
+          tableRow.classList.remove('hidden');
+          tableCol.classList.add('hidden');
+          activeRow.classList.add('active')
+          activeCol.classList.remove('active');
+                  
+        let innerHTML = `
+        <tr>
+            <td>
+                <a href="#" class="text-body">${medicineKey}</a>
+            </td>
+            <td>
+                <span class="badge badge-soft-success mb-0">${description}</span>
+            </td>
+            <td>${remarks}</td>
+            <td>${schedule}</td>
+            <td>${duration}</td>
+            <td>
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item">
+                <a href="javascript:void(0);" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit"
+                  class="px-2 text-primary"><i class="bx bx-pencil font-size-18"></i></a>
+              </li>
+              <li class="list-inline-item">
+                <a href="javascript:void(0);" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"
+                  class="px-2 text-danger"><i class="bx bx-trash-alt font-size-18"></i></a>
+              </li>
+            </ul>
+          </td>
+        </tr>`;
+    tableBody.innerHTML += innerHTML;
+          
+        } else if (option === 'row') {
+          tableRow.classList.add('hidden');
+          tableCol.classList.remove('hidden');
+          activeRow.classList.remove('active')
+          activeCol.classList.add('active')
+          let innerHTML = `
+          <tr>
+          <tr>
+            <th scope="col">Name</th>
+            <td>
+              <a href="#" class="text-body">${medicineKey}</a>
+            </td>
+          </tr>
+          <tr>
+            <th scope="col">Description</th>
+            <td>
+              <span class="badge badge-soft-success mb-0">${description}</span>
+            </td>
+          </tr>
+          <tr>
+            <th scope="col">Remarks</th>
+            <td>${remarks}</td>
+          </tr>
+          <tr>
+            <th scope="col">Schedule</th>
+            <td>${schedule}</td>
+          </tr>
+          <tr>
+            <th scope="col">Duration</th>
+            <td>${duration}</td>
+          </tr>
+          <tr>
+            <th scope="col">Action</th>
+            <td>
+              <ul class="list-inline mb-0">
+                <li class="list-inline-item">
+                  <a href="javascript:void(0);" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit"
+                    class="px-2 text-primary"><i class="bx bx-pencil font-size-18"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="javascript:void(0);" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"
+                    class="px-2 text-danger"><i class="bx bx-trash-alt font-size-18"></i></a>
+                </li>
+              </ul>
+            </td>
+          </tr>
+          </tr>
+          <tr>
+            <th colspan="2" style="border: none;"></th>
+          </tr>
+          `
+          rowTableContent.innerHTML += innerHTML;
+        }
+    }
+
+  }
   const fileInput = document.getElementById("fileInput");
   const openCameraBtn = document.getElementById("openCameraBtn");
 
